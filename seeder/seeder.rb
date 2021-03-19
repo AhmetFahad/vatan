590fc9140bf1ed4beee20e86040c0b30d7aeffaa
Ahmet Fahad <ahmadfahad6394@gmail.com>
2021-03-20 00:39:33 +0300

    TEST

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,5 +2,6 @@ pages << {
     page_type: "listings",
     method: "GET",
     url: "https://www.vatanbilgisayar.com/bilgisayar",
-    fetch_type: "browser"
+    fetch_type: "browser",
+    force_fetch: true
 }
